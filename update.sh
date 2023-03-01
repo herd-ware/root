0db2e09cc07534cd7427e7904eb36ccb0c5a31f1
Mathieu Escouteloup <mathieu.escouteloup@gmail.com>
2023-03-01 17:20:10 +0100

    Renames old content (except Dome)

---
+++ update.sh
@@ -0,0 +1,37 @@
+PRJ_DIR=/home/escou64/herd-ware/root/main
+
+git checkout main
+git pull origin main
+git submodule update
+
+cd ${PRJ_DIR}/hw/common
+git checkout main
+git pull origin main
+
+cd ${PRJ_DIR}/hw/core/aubrac
+git checkout main
+git pull origin main
+
+cd ${PRJ_DIR}/hw/core/abondance
+git checkout main
+git pull origin main
+
+cd ${PRJ_DIR}/hw/mem/hay
+git checkout main
+git pull origin main
+
+cd ${PRJ_DIR}/hw/io
+git checkout main
+git pull origin main
+
+cd ${PRJ_DIR}/hw/pltf/cheese
+git checkout main
+git pull origin main
+
+cd ${PRJ_DIR}/sw/isa-tests
+git checkout main
+git pull origin main
+
+cd ${PRJ_DIR}/tools
+git checkout main
+git pull origin main

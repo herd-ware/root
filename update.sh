771ec2589767a03a92f36a85c22e912dbafe4fe4
Mathieu Escouteloup <mathieu.escouteloup@gmail.com>
2023-03-06 16:38:43 +0100

    Modifies Gitlab -> Github dependencies

--- update.sh
+++ update.sh
@@ -1,37 +1,37 @@
-PRJ_DIR=/home/escou64/herd-ware/root/main
+PRJ_DIR=/home/escou64/herd-ware/root/public
 
-git checkout main
-git pull origin main
+git checkout public
+git pull origin public
 git submodule update
 
 cd ${PRJ_DIR}/hw/common
-git checkout main
-git pull origin main
+git checkout public
+git pull origin public
 
 cd ${PRJ_DIR}/hw/core/aubrac
-git checkout main
-git pull origin main
+git checkout public
+git pull origin public
 
 cd ${PRJ_DIR}/hw/core/abondance
-git checkout main
-git pull origin main
+git checkout public
+git pull origin public
 
 cd ${PRJ_DIR}/hw/mem/hay
-git checkout main
-git pull origin main
+git checkout public
+git pull origin public
 
 cd ${PRJ_DIR}/hw/io
-git checkout main
-git pull origin main
+git checkout public
+git pull origin public
 
 cd ${PRJ_DIR}/hw/pltf/cheese
-git checkout main
-git pull origin main
+git checkout public
+git pull origin public
 
 cd ${PRJ_DIR}/sw/isa-tests
-git checkout main
-git pull origin main
+git checkout public
+git pull origin public
 
 cd ${PRJ_DIR}/tools
-git checkout main
-git pull origin main
+git checkout public
+git pull origin public

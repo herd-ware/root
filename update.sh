150e629191bddb6ba9c979055dce8fca5737fcf6
Mathieu Escouteloup <mathieu.escouteloup@gmail.com>
2023-03-08 09:54:32 +0100

    Updates submodules README.md

--- update.sh
+++ update.sh
@@ -1,37 +1,37 @@
-PRJ_DIR=/home/escou64/herd-ware/root/public
+PRJ_DIR=${HERDWARE_ROOT_PATH}
 
-git checkout public
-git pull origin public
+git checkout main
+git pull origin main
 git submodule update
 
 cd ${PRJ_DIR}/hw/common
-git checkout public
-git pull origin public
+git checkout main
+git pull origin main
 
 cd ${PRJ_DIR}/hw/core/aubrac
-git checkout public
-git pull origin public
+git checkout main
+git pull origin main
 
 cd ${PRJ_DIR}/hw/core/abondance
-git checkout public
-git pull origin public
+git checkout main
+git pull origin main
 
 cd ${PRJ_DIR}/hw/mem/hay
-git checkout public
-git pull origin public
+git checkout main
+git pull origin main
 
 cd ${PRJ_DIR}/hw/io
-git checkout public
-git pull origin public
+git checkout main
+git pull origin main
 
 cd ${PRJ_DIR}/hw/pltf/cheese
-git checkout public
-git pull origin public
+git checkout main
+git pull origin main
 
 cd ${PRJ_DIR}/sw/isa-tests
-git checkout public
-git pull origin public
+git checkout main
+git pull origin main
 
 cd ${PRJ_DIR}/tools
-git checkout public
-git pull origin public
+git checkout main
+git pull origin main
